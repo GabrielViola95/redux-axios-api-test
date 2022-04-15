9eb1448cdaa6daafd933c62103842d266e39290e
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-04-15 18:56:29 -0300

    adding basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from './components/ProductListing';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<h1 style={{marginTop: "5rem", marginLeft: "5rem"}}>404 not Found</h1>}/>
+      </Routes>
+    </Router>
   );
 }
 
