881ba884bff8c74f92825c819b00a6d6e91c007a
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-04-15 23:04:41 -0300

    descriptions

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
     const renderList = products.map((product) => {
         const { id, title, image, price, category } = product;
         return (
-            <div className='four column wide'>
+            <div className='four wide columns' key={id}>
+                <Link to={`/product/${id}`}>
         <div className='ui link cards'>
             <div className="card">
                 <div className="image">
-                    <img src={image} alt="image product" />
+                    <img src={image} alt={title} />
                 </div>
                 <div className="content">
                     <div className="header">{title}</div>
@@ -19,11 +21,15 @@ const ProductComponent = () => {
                 </div>
             </div>
         </div>
+        </Link>
              </div>
         )
     })
 
     return (
+        <>
+            {renderList}
+        </>
   )
 }
 
